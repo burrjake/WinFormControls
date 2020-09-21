f8889e5a383ae720d4a07535aec4dff4429e4d04
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-21 08:28:33 -0600

    Add project files.

---
+++ WInFormControlsForm.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WInFormControlsForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ExamplePictureBox = New System.Windows.Forms.PictureBox()
+        Me.GoButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupOne3CheckBox = New System.Windows.Forms.CheckBox()
+        Me.GroupOne3RadioButton = New System.Windows.Forms.RadioButton()
+        Me.GroupOne2CheckBox = New System.Windows.Forms.CheckBox()
+        Me.GroupOne1CheckBox = New System.Windows.Forms.CheckBox()
+        Me.GroupOne2RadioButton = New System.Windows.Forms.RadioButton()
+        Me.GroupOne1RadioButton = New System.Windows.Forms.RadioButton()
+        Me.LastNameLabel = New System.Windows.Forms.Label()
+        Me.FirstNameLabel = New System.Windows.Forms.Label()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        CType(Me.ExamplePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ExamplePictureBox
+        '
+        Me.ExamplePictureBox.Location = New System.Drawing.Point(418, 44)
+        Me.ExamplePictureBox.Name = "ExamplePictureBox"
+        Me.ExamplePictureBox.Size = New System.Drawing.Size(353, 292)
+        Me.ExamplePictureBox.TabIndex = 0
+        Me.ExamplePictureBox.TabStop = False
+        '
+        'GoButton
+        '
+        Me.GoButton.Location = New System.Drawing.Point(271, 361)
+        Me.GoButton.Name = "GoButton"
+        Me.GoButton.Size = New System.Drawing.Size(246, 77)
+        Me.GoButton.TabIndex = 1
+        Me.GoButton.Text = "Go"
+        Me.GoButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(523, 361)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(248, 77)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(148, 363)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(100, 21)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(148, 390)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(100, 21)
+        Me.CheckBox2.TabIndex = 4
+        Me.CheckBox2.Text = "CheckBox2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(148, 417)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(100, 21)
+        Me.CheckBox3.TabIndex = 5
+        Me.CheckBox3.Text = "CheckBox3"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(25, 362)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton1.TabIndex = 6
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(25, 389)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton2.TabIndex = 7
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(25, 416)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton3.TabIndex = 8
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupOne3CheckBox)
+        Me.GroupBox1.Controls.Add(Me.GroupOne3RadioButton)
+        Me.GroupBox1.Controls.Add(Me.GroupOne2CheckBox)
+        Me.GroupBox1.Controls.Add(Me.GroupOne1CheckBox)
+        Me.GroupBox1.Controls.Add(Me.GroupOne2RadioButton)
+        Me.GroupBox1.Controls.Add(Me.GroupOne1RadioButton)
+        Me.GroupBox1.Controls.Add(Me.LastNameLabel)
+        Me.GroupBox1.Controls.Add(Me.FirstNameLabel)
+        Me.GroupBox1.Controls.Add(Me.LastNameTextBox)
+        Me.GroupBox1.Controls.Add(Me.FirstNameTextBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 44)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(375, 286)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Custumer Info"
+        '
+        'GroupOne3CheckBox
+        '
+        Me.GroupOne3CheckBox.AutoSize = True
+        Me.GroupOne3CheckBox.Location = New System.Drawing.Point(215, 205)
+        Me.GroupOne3CheckBox.Name = "GroupOne3CheckBox"
+        Me.GroupOne3CheckBox.Size = New System.Drawing.Size(100, 21)
+        Me.GroupOne3CheckBox.TabIndex = 12
+        Me.GroupOne3CheckBox.Text = "CheckBox4"
+        Me.GroupOne3CheckBox.UseVisualStyleBackColor = True
+        '
+        'GroupOne3RadioButton
+        '
+        Me.GroupOne3RadioButton.AutoSize = True
+        Me.GroupOne3RadioButton.Location = New System.Drawing.Point(65, 205)
+        Me.GroupOne3RadioButton.Name = "GroupOne3RadioButton"
+        Me.GroupOne3RadioButton.Size = New System.Drawing.Size(115, 21)
+        Me.GroupOne3RadioButton.TabIndex = 16
+        Me.GroupOne3RadioButton.TabStop = True
+        Me.GroupOne3RadioButton.Text = "RadioButton4"
+        Me.GroupOne3RadioButton.UseVisualStyleBackColor = True
+        '
+        'GroupOne2CheckBox
+        '
+        Me.GroupOne2CheckBox.AutoSize = True
+        Me.GroupOne2CheckBox.Location = New System.Drawing.Point(215, 179)
+        Me.GroupOne2CheckBox.Name = "GroupOne2CheckBox"
+        Me.GroupOne2CheckBox.Size = New System.Drawing.Size(100, 21)
+        Me.GroupOne2CheckBox.TabIndex = 11
+        Me.GroupOne2CheckBox.Text = "CheckBox5"
+        Me.GroupOne2CheckBox.UseVisualStyleBackColor = True
+        '
+        'GroupOne1CheckBox
+        '
+        Me.GroupOne1CheckBox.AutoSize = True
+        Me.GroupOne1CheckBox.Location = New System.Drawing.Point(215, 152)
+        Me.GroupOne1CheckBox.Name = "GroupOne1CheckBox"
+        Me.GroupOne1CheckBox.Size = New System.Drawing.Size(100, 21)
+        Me.GroupOne1CheckBox.TabIndex = 10
+        Me.GroupOne1CheckBox.Text = "CheckBox6"
+        Me.GroupOne1CheckBox.UseVisualStyleBackColor = True
+        '
+        'GroupOne2RadioButton
+        '
+        Me.GroupOne2RadioButton.AutoSize = True
+        Me.GroupOne2RadioButton.Location = New System.Drawing.Point(65, 178)
+        Me.GroupOne2RadioButton.Name = "GroupOne2RadioButton"
+        Me.GroupOne2RadioButton.Size = New System.Drawing.Size(115, 21)
+        Me.GroupOne2RadioButton.TabIndex = 15
+        Me.GroupOne2RadioButton.TabStop = True
+        Me.GroupOne2RadioButton.Text = "RadioButton5"
+        Me.GroupOne2RadioButton.UseVisualStyleBackColor = True
+        '
+        'GroupOne1RadioButton
+        '
+        Me.GroupOne1RadioButton.AutoSize = True
+        Me.GroupOne1RadioButton.Location = New System.Drawing.Point(65, 151)
+        Me.GroupOne1RadioButton.Name = "GroupOne1RadioButton"
+        Me.GroupOne1RadioButton.Size = New System.Drawing.Size(115, 21)
+        Me.GroupOne1RadioButton.TabIndex = 14
+        Me.GroupOne1RadioButton.TabStop = True
+        Me.GroupOne1RadioButton.Text = "RadioButton6"
+        Me.GroupOne1RadioButton.UseVisualStyleBackColor = True
+        '
+        'LastNameLabel
+        '
+        Me.LastNameLabel.AutoSize = True
+        Me.LastNameLabel.Location = New System.Drawing.Point(1, 88)
+        Me.LastNameLabel.Name = "LastNameLabel"
+        Me.LastNameLabel.Size = New System.Drawing.Size(76, 17)
+        Me.LastNameLabel.TabIndex = 13
+        Me.LastNameLabel.Text = "Last Name"
+        '
+        'FirstNameLabel
+        '
+        Me.FirstNameLabel.AutoSize = True
+        Me.FirstNameLabel.Location = New System.Drawing.Point(6, 50)
+        Me.FirstNameLabel.Name = "FirstNameLabel"
+        Me.FirstNameLabel.Size = New System.Drawing.Size(76, 17)
+        Me.FirstNameLabel.TabIndex = 12
+        Me.FirstNameLabel.Text = "First Name"
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Location = New System.Drawing.Point(83, 88)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(274, 22)
+        Me.LastNameTextBox.TabIndex = 11
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(83, 50)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(274, 22)
+        Me.FirstNameTextBox.TabIndex = 10
+        '
+        'WInFormControlsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.GoButton)
+        Me.Controls.Add(Me.ExamplePictureBox)
+        Me.Name = "WInFormControlsForm"
+        Me.Text = "Controls Example"
+        CType(Me.ExamplePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ExamplePictureBox As PictureBox
+    Friend WithEvents GoButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupOne3CheckBox As CheckBox
+    Friend WithEvents GroupOne3RadioButton As RadioButton
+    Friend WithEvents GroupOne2CheckBox As CheckBox
+    Friend WithEvents GroupOne1CheckBox As CheckBox
+    Friend WithEvents GroupOne2RadioButton As RadioButton
+    Friend WithEvents GroupOne1RadioButton As RadioButton
+    Friend WithEvents LastNameLabel As Label
+    Friend WithEvents FirstNameLabel As Label
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents FirstNameTextBox As TextBox
+End Class
---
+++ WInFormControlsForm.vb
@@ -0,0 +1,3 @@
+﻿Public Class WInFormControlsForm
+
+End Class
