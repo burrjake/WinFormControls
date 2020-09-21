fb8f8d2e77c3e94051b8b21ac84d5867a1b19765
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-21 08:54:46 -0600

    add example code

--- WInFormControlsForm.vb
+++ WInFormControlsForm.vb
@@ -1,3 +1,21 @@
 ﻿Public Class WInFormControlsForm
 
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub GoButton_Click(sender As Object, e As EventArgs) Handles GoButton.Click
+        If ExamplePictureBox.Visible = False Then
+            ExamplePictureBox.Show()
+            ExitButton.Enabled = False
+        Else
+            ExamplePictureBox.Hide()
+            ExitButton.Enabled = True
+        End If
+        If GroupOne1RadioButton.Checked = True Then
+            ExamplePictureBox.BackColor = System.Drawing.SystemColors.Desktop
+        Else
+            ExamplePictureBox.BackColor = System.Drawing.SystemColors.Control
+        End If
+    End Sub
 End Class
